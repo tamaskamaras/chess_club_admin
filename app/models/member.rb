d039451dfe6fa6a872449fe795d346a0930c063c
Tamas Kamaras <kamaras.tamas.viktor@gmail.com>
2022-06-15 19:56:10 +0200

    add dependent: :destroy to Member-Match

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Member < ApplicationRecord
-  has_many :winnings, foreign_key: :winner_id, class_name: 'Match'
-  has_many :losses, foreign_key: :loser_id, class_name: 'Match'
+  has_many :winnings, foreign_key: :winner_id, class_name: 'Match', dependent: :destroy
+  has_many :losses, foreign_key: :loser_id, class_name: 'Match', dependent: :destroy
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
