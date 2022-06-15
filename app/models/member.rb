8abc44663bb77fb7dc8226f680221f249a1a8ec9
Tamas Kamaras <kamaras.tamas.viktor@gmail.com>
2022-06-15 20:37:40 +0200

    autoincrement Member#rank

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,9 +4,21 @@ class Member < ApplicationRecord
   has_many :winnings, foreign_key: :winner_id, class_name: 'Match', dependent: :destroy
   has_many :losses, foreign_key: :loser_id, class_name: 'Match', dependent: :destroy
 
+  before_save :set_rank, if: :rank_blank?
+
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def matches
     Match.where('winner_id = :self_id OR loser_id = :self_id', self_id: id)
+  end
+
+  private
+
+  def rank_blank?
+    rank.blank?
+  end
+
+  def set_rank
+    self.rank = Member.count + 1
   end
 end
