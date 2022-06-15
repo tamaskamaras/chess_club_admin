117bae12e9d23248cd21b58033c80789ca539493
Tamas Kamaras <kamaras.tamas.viktor@gmail.com>
2022-06-15 19:44:33 +0200

    implement Member#matches

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,8 @@ class Member < ApplicationRecord
   has_many :losses, foreign_key: :loser_id, class_name: 'Match'
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def matches
+    Match.where('winner_id = :self_id OR loser_id = :self_id', self_id: id)
+  end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -3,5 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Member do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#matches' do
+    context 'when 1 member had 2 matches' do
+      subject(:member1) { create(:member) }
+
+      let(:match1) { create(:match, winner: member1) }
+      let(:match2) { create(:match, loser: member1) }
+
+      it 'returns both' do
+        expect(member1.matches).to contain_exactly(match1, match2)
+      end
+    end
+
+    context 'when member had no matches' do
+      subject(:member1) { create(:member) }
+
+      it 'returns both' do
+        expect(member1.matches).to eq([])
+      end
+    end
+  end
 end
