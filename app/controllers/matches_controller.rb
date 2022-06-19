178f0730412e622fc6204d2cd5b96fe8810d8e84
Tamas Kamaras <kamaras.tamas.viktor@gmail.com>
2022-06-19 21:30:31 +0200

    add matches#index

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class MatchesController < ApplicationController
+  def index
+    @matches =
+      Match
+        .joins(:player_a, :player_b, :winner)
+        .pluck(
+          'members.first_name',
+          'player_bs_matches.first_name',
+          'winners_matches.first_name',
+          'matches.created_at'
+        )
+  end
 end
